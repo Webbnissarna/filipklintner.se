548f8886bf0a9ddafd7989ba3a731e03d4a441ca
kjof <andreeastroem@gmail.com>
2021-11-28 23:04:59 +0100

    feat: add frame border

--- src/gatsby/wrapPageElement.tsx
+++ src/gatsby/wrapPageElement.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Box } from "@theme-ui/components";
+
 import Header from "../components/Header";
 
 import { GatsbyBrowser, GatsbySSR } from "gatsby";
@@ -8,11 +10,24 @@ type WrapPageElement =
   | GatsbyBrowser["wrapPageElement"]
   | GatsbySSR["wrapPageElement"];
 
-export const wrapPageElement: WrapPageElement = ({ element, props }: never) => {
+export const wrapPageElement: WrapPageElement = ({ element }: never) => {
   return (
     <div>
-      <Header />
-      <main>{element}</main>
+      <Box
+        sx={{
+          borderStyle: "solid",
+          borderColor: "brown",
+          borderWidth: 3,
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+        }}
+      >
+        <Header />
+        <main>{element}</main>
+      </Box>
     </div>
   );
 };
