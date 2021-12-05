967eaf6eee7e437e4244f6d2158b37d00fc1946e
kjof <andreeastroem@gmail.com>
2021-12-05 21:14:52 +0100

    feat: add hero image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,32 @@ import { Box, Flex } from "@theme-ui/components";
 import { jsx } from "@theme-ui/core";
 import MenuButton from "../components/MenuButton/MenuButton";
 
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+
 // markup
+export default function IndexPage() {
+  const pageQuery = useStaticQuery(
+    graphql`
+      query landingpage {
+        datoCmsLandingpage(originalId: { eq: "79166934" }) {
+          title
+          description
+          originalId
+          heroImage {
+            gatsbyImageData(placeholder: BLURRED)
+            alt
+          }
+        }
+      }
+    `
+  );
+
+  const pageData = pageQuery?.datoCmsLandingpage;
+
+  console.log("page data", pageData);
+
+  //states
 const IndexPage = () => {
   return (
     <Flex
@@ -17,8 +42,14 @@ const IndexPage = () => {
       <Box sx={{ cursor: "pointer" }}>
         <MenuButton></MenuButton>
       </Box>
+      {pageData?.heroImage && (
+        <Box sx={{ padding: "3xl" }}>
+          <GatsbyImage
+            image={pageData?.heroImage?.gatsbyImageData}
+            alt={pageData?.heroImage?.alt}
+          />
+        </Box>
+      )}
     </Flex>
   );
-};
-
-export default IndexPage;
+}
